0986fea038266b0e8e8e772b7284949d5d032bcb
Bikash-Dushad <bikashghll@gmail.com>
2024-07-05 00:07:24 +0530

    css done and home  page done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,11 +5,11 @@ const Product = require('../models/product');
 // this function takes user to home
 module.exports.home = async function(req, res) {
     if(req.user){
-        let products = await Product.find({}); 
+        let product = await Product.find({}); 
         
         return res.render('home', {
             title : "Home",
-            products : products,
+            product : product,
         })
     }else{
         return res.render('home', {
